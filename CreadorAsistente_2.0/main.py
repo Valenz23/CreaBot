d58033db857fd5f87f74d1d887a0eb467eda2c99
Valenz23 <pva_89@hotmail.com>
2022-07-18 09:22:36 +0200

    checheck
    añadido chequeo de datos antes de crear el bot

--- CreadorAsistente_2.0/main.py
+++ CreadorAsistente_2.0/main.py
@@ -1,8 +1,27 @@
+from distutils.errors import LibError
 from encodings import utf_8
+from tkinter import Scrollbar
 import PySimpleGUI as sg
 import os
 
 sg.theme('DefaultNoMoreNagging')
+
+
+
+#######################
+###    VARIABLES    ###
+#######################
+
+fichero = ""
+algoritmo = "Jaro-Winkler"
+nombrebot = ""
+location = ""
+tipo = "Árbol de decisión"
+minlim = 0.5
+maxlim = 0.7
+examples = 20
+
+
 
 ###################################################
 ###     DISEÑO      DE      LA      INTERFAZ    ###
@@ -63,18 +82,223 @@ layout = [
 
 window = sg.Window("Creador de Asistentes con RASA", layout, size=(1400, 900))
 
-###########################################################
-###     FUNCIONAMIENTO      DE      LOS      BOTONES    ###
-###########################################################
 
-fichero = ""
-algoritmo = "Jaro-Winkler"
-nombrebot = ""
-location = ""
-tipo = "Árbol de decisión"
-minlim = 0.5
-maxlim = 0.7
-examples = 20
+
+#################################
+# FUNCION PARA CHEQUEAR DATOS ###
+#################################
+
+def checkcheck():
+
+    estado = False
+    tiene_entitities = False
+    tiene_intents = False
+    tiene_responses = False
+    tiene_stories = False
+    errores = ""
+    c = 0
+
+    if fichero == "": 
+        sg.PopupOK("Falta el fichero XML", title="Aviso")  
+    else:        
+        with open (fichero,'rt',encoding='utf8') as contenido:  # abro el fichero
+            for linea in contenido:                             # y voy mirando linea por linea
+
+                linea = linea.replace("\n","").lower()          # quito los saltos de linea
+                c = c+1                                         # cuento las lineas
+
+                # segun el tipo de bot, se deben chequear distintas cosas                
+                if tipo == "Preguntas Frecuentes":                    
+                    if linea.startswith("entities") or linea.startswith("entity"):                                  # no aceptamos entidades
+                        errores += "Error: linea {}. En el bot de Preguntas Frecuentes no son necesarias entities\n\n".format(c)
+
+                    elif linea.startswith("response"):                                                              # ni responses
+                        errores += "Error: linea {}. En el bot de Preguntas Frecuentes no son necesarios responses\n\n".format(c)
+
+                    elif linea.startswith("intent"):
+                        linea = linea.replace("intent:","")
+
+                        if len(linea.split(";")) == 3:          # para este bot, necesito que tengan tres argumentos los intents
+                            error = False                       
+
+                            for item in linea.split(";"):
+                                #busco los parentesis
+                                if item.startswith("("):
+                                    para = item.replace("(","").replace(")","")
+                                    # miro si esta vacio
+                                    if para == "":
+                                        error = True
+                                        errores += "Error: Linea{}. Parametro entre parentesis sin valor\n\n".format(c)
+                                #busco las llaves
+                                elif item.startswith("{"):                                
+                                    para = item.replace("{","").replace("}","")
+                                    # miro si esta vacio
+                                    if para == "":
+                                        error = True
+                                        errores += "Error: Linea{}. Parametro entre llaves sin valor\n\n".format(c)
+                                else:
+                                    if item == "":
+                                        error = True
+                                        errores += "Error: Linea{}. No hay valor\n\n".format(c)
+                                
+                            if not error:       # si no ha habido ningun fallo en la linea, tiene tres argumentos, uno normal, otro entre parentesis y otro entre llaves
+                                tiene_intents = True
+
+                        else:
+                            errores += "Error: linea {}. Error en los argumentos, chequee los separadores. El bot de Preguntas Frecuentes requiere 3 argumentos en los intents.\n\n".format(c)
+                            
+                    elif linea.startswith("story"):
+                        linea = linea.replace("story:","")
+
+                        if len(linea.split(";")) == 1:          # para este bot, necesito que tenga un argumento para los story                            
+                            if linea == "":
+                                errores += "Error: Linea{}. No hay valor\n\n".format(c)
+                            else:
+                                tiene_stories = True
+                        else:
+                            errores += "Error: linea {}. Error en los argumentos, chequee los separadores. El bot de Preguntas Frecuentes requiere 1 argumento en los story.\n\n".format(c)
+
+                ######################################################
+
+                elif tipo == "Secuencia de Pasos":
+
+                    if linea.startswith("entities") or linea.startswith("entity"):              # tienen que haber entidades
+                        if linea.startswith("entities"):
+                            linea = linea.replace("entities:","")
+                            if not ";" in linea:                    
+                                errores += "Error: Linea{}. Error en los separadores, asegurese de que son ';'\n\n".format(c)
+                            else:
+                                tiene_entitities = True
+                        else:
+                            linea = linea.replace("entity","")
+                            if ";" in linea:                    
+                                errores += "Error: Linea{}. No hacen falta separadores en esta linea\n\n".format(c)
+                            else:
+                                tiene_entitities = True
+
+                    elif linea.startswith("response"):                                  # no responses
+                        errores += "Error: linea {}. En el bot de Secuencia de Pasos no son necesarios responses\n\n".format(c)
+
+                    elif linea.startswith("intent"):
+                        linea = linea.replace("intent:","")
+
+                        if len(linea.split(";")) == 2:          # para este bot, necesito que tengan dos argumentos los intents
+                            error = False                       
+
+                            for item in linea.split(";"):                                
+                                #busco las llaves
+                                if item.startswith("{"):                                
+                                    para = item.replace("{","").replace("}","")
+                                    # miro si esta vacio
+                                    if para == "":
+                                        error = True
+                                        errores += "Error: Linea{}. Parametro entre llaves sin valor\n\n".format(c)
+                                else:
+                                    if item == "":
+                                        error = True
+                                        errores += "Error: Linea{}. No hay valor\n\n".format(c)
+                                
+                            if not error:       # si no ha habido ningun fallo en la linea, tiene dos argumentos, uno normal y otro entre llaves
+                                tiene_intents = True
+                        else:
+                            errores += "Error: linea {}. Error en los argumentos, chequee los separadores. El bot de Secuencia de Pasos requiere 2 argumentos en los intents.\n\n".format(c)
+
+                    elif linea.startswith("story"):                                     # tompooco hay stories
+                        errores += "Error: linea {}. En el bot de Secuencia de Pasos no son necesarios stories\n\n".format(c)
+                
+                ######################################################
+
+                elif tipo == "Árbol de decisión":
+                    if linea.startswith("entities") or linea.startswith("entity"):   # no se aceptan entidades
+                        errores += "Error: linea {}. En el bot de Arbol de Decision no son necesarias entities\n\n".format(c)
+
+                    elif linea.startswith("response"):  
+                        linea = linea.replace("response:","")
+                                        
+                        if len(linea.split(";")) == 2:                              
+                            error = False            
+                            
+                            for item in linea.split(";"):
+                                #busco los parentesis
+                                if item.startswith("("):
+                                    para = item.replace("(","").replace(")","")
+                                    # miro si esta vacio
+                                    if para == "":
+                                        error = True
+                                        errores += "Error: Linea{}. Parametro entre parentesis sin valor\n\n".format(c)
+                                else:
+                                    if item == "":
+                                        error = True
+                                        errores += "Error: Linea{}. No hay valor\n\n".format(c)
+                            
+                            if not error:       # si no ha habido ningun fallo en la linea, tiene dos argumentos, uno normal y otro entre parentesis
+                                tiene_responses = True
+                        else:
+                            errores += "Error: linea {}. Error en los argumentos, chequee los separadores. El bot de Arbol de Decision requiere 2 argumentos en los response.\n\n".format(c)
+
+                        
+                    elif linea.startswith("intent"):
+                        linea = linea.replace("intent:","")
+
+                        if len(linea.split(";")) == 2 or len(linea.split(";")) == 3:          # para este bot, necesito que tengan tres argumentos los intents
+                            error = False                       
+
+                            for item in linea.split(";"):
+                                #busco los parentesis
+                                if item.startswith("("):
+                                    para = item.replace("(","").replace(")","")
+                                    # miro si esta vacio
+                                    if para == "":
+                                        error = True
+                                        errores += "Error: Linea{}. Parametro entre parentesis sin valor\n\n".format(c)
+                                #busco las llaves
+                                elif item.startswith("{"):                                
+                                    para = item.replace("{","").replace("}","")
+                                    # miro si esta vacio
+                                    if para == "":
+                                        error = True
+                                        errores += "Error: Linea{}. Parametro entre llaves sin valor\n\n".format(c)
+                                else:
+                                    if item == "":
+                                        error = True
+                                        errores += "Error: Linea{}. No hay valor\n\n".format(c)
+                                
+                            if not error:       # si no ha habido ningun fallo en la linea, tiene tres argumentos, uno normal, otro entre parentesis y otro entre llaves
+                                tiene_intents = True
+
+                        else:
+                            errores += "Error: linea {}. Error en los argumentos, chequee los separadores. El bot de Arbol de decision requiere 2 o 3 argumentos en los intents.\n\n".format(c)
+
+                    elif linea.startswith("story"):
+                        linea = linea.replace("story:","")
+                        
+                        if not ";" in linea:                    
+                            errores += "Error: Linea{}. Error en los separadores, chequee los separadores, asegurese de que son ';'\n\n".format(c)                        
+                        else:
+                            tiene_stories = True
+
+            
+    # comprobamos por ultimo si se cumplen las ultimas condiciones    
+    if tipo == "Preguntas Frecuentes" and not tiene_intents and not tiene_stories:
+        errores += "El bot de Preguntas Frecuentes necesita intents y stories"
+    elif tipo == "Secuencia de Pasos" and not tiene_entitities and not tiene_intents:
+        errores += "El bot de Secuencia de Pasos necesita entities e intents"
+    elif tipo == "Árbol de decisión" and not tiene_intents and not tiene_responses and not tiene_stories:
+        errores += "El bot de Arbol de decision necesita intents, responses y stories"
+    
+    # si no hay errores, se puede crear el bot
+    if errores == "":
+        estado=True    
+    else:
+        sg.popup_scrolled(errores, title="Aviso")
+
+    return estado
+
+
+
+###############################
+###     CODIGO PRINCIPAL    ###
+###############################
 
 while True:
     event, values = window.read()
@@ -146,24 +370,25 @@ while True:
 
     if event == "-OK-":                                         # crear el asistente
         try:
-            if fichero == "":
-                sg.PopupOK("Falta el fichero XML", title="Aviso")
-            elif algoritmo == "":
-                sg.PopupOK("Falta el algoritmo", title="Aviso")
-            elif nombrebot == "":
-                sg.PopupOK("Falta el nombre del asistente", title="Aviso")
-            elif location == "":
-                sg.PopupOK("Falta el destino del asistente", title="Aviso")
-            else:                
-                click = sg.PopupOKCancel("Se creará el asistente {}\nEn la carpeta {}\n\nPor favor dirijase a la consola".format(nombrebot, location), title="Aviso")
-                #creacion del bot
-                if click == "OK": # segun el tipo de asistente   
-                    if tipo == "Preguntas Frecuentes" or tipo == "Árbol de decisión":  
-                        os.system("python bot_builder.py {} {} {} {} {} {} {}".format(fichero, algoritmo, nombrebot, location, minlim, maxlim, examples))   
-                    if tipo == "Secuencia de Pasos":
-                        os.system("python bot_builder2.py {} {} {} {} {} {} {}".format(fichero, algoritmo, nombrebot, location, minlim, maxlim, examples))     
-                    
-                    sg.PopupOK("Asistente creado", title="Aviso")       
+            if checkcheck():
+                if fichero == "":
+                    sg.PopupOK("Falta el fichero XML", title="Aviso")
+                elif algoritmo == "":
+                    sg.PopupOK("Falta el algoritmo", title="Aviso")
+                elif nombrebot == "":
+                    sg.PopupOK("Falta el nombre del asistente", title="Aviso")
+                elif location == "":
+                    sg.PopupOK("Falta el destino del asistente", title="Aviso")
+                else:                
+                    click = sg.PopupOKCancel("Se creará el asistente {}\nEn la carpeta {}\n\nPor favor dirijase a la consola".format(nombrebot, location), title="Aviso")
+                    #creacion del bot
+                    if click == "OK": # segun el tipo de asistente   
+                        if tipo == "Preguntas Frecuentes" or tipo == "Árbol de decisión":  
+                            os.system("python bot_builder.py {} {} {} {} {} {} {}".format(fichero, algoritmo, nombrebot, location, minlim, maxlim, examples))   
+                        if tipo == "Secuencia de Pasos":
+                            os.system("python bot_builder2.py {} {} {} {} {} {} {}".format(fichero, algoritmo, nombrebot, location, minlim, maxlim, examples))     
+                        
+                        sg.PopupOK("Asistente creado", title="Aviso")       
                                             
         except:
             sg.PopupOK("Se produjo un error", title="Aviso")
